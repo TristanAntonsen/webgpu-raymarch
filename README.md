31c809819751d8c37bef6e06dd77da2d3a9f262c
TristanAntonsen <tristan.antonsen@gmail.com>
2023-07-05 19:40:48 -0500

    Reading from .wgsl file and making resolution a uniform

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Working through Google's WebGPU tutorial (Conway's Game of life)
+# Creating a basic ray marcher using WebGPU
 
+This is a very setup to render a WebGPU fragment shader to a canvas element. This was created for experimentation with WebGPU/wgsl and ray marching.
+
+
+WebGPU setup based primarily off of:
 https://codelabs.developers.google.com/your-first-webgpu-app#1
+
+
+### References:
+
+- [Inigo Quilez - Ray Marching SDFs](https://iquilezles.org/articles/raymarchingdf/  )
+- [Inigo Quilez - Distance Functions](https://iquilezles.org/articles/distfunctions/)
+- [SimonDev - Ray Marching](https://www.youtube.com/watch?v=BNZtUB7yhX4&t=146s)
+- [The Art of Code - Ray Marching](https://www.youtube.com/watch?v=PGtv-dBi2wE&t=733s)
--- main.js
+++ main.js
@@ -1,35 +1,52 @@
-console.log("Pushing pixels...")
-// https://codelabs.developers.google.com/your-first-webgpu-app
 
-const canvas = document.querySelector("canvas");
+async function run() {
+  // Loading shader from .wgsl file
+  const shaderText = await fetch('./shader.wgsl')
+    .then(result => result.text());
+    
+  // Rendering texture to the canvas
+  render(shaderText)
+}
 
-// Setting up the GPU Pipeline
+// Getting the canvas & setting the resolution
+let canvas = document.querySelector("canvas");
 
-// Checking to make sure browser supports WebGPU
-if (!navigator.gpu) {
+const X_RES = 512;
+const Y_RES = 512;
+
+canvas.width = X_RES;
+canvas.height = Y_RES;
+
+// Most of this setup is pulled from Google's webgpu-for-beginners tutorial setup (Conway's Game of Life)
+async function render(shaderText) {
+
+  // Setting up the GPU Pipeline
+
+  // Checking to make sure browser supports WebGPU
+  if (!navigator.gpu) {
     throw new Error("WebGPU not supported on this browser.");
-}
+  }
 
-// WebGPU's representation of the available gpu hardware
-const adapter = await navigator.gpu.requestAdapter(); // returns a promise, so use await
-if (!adapter) {
+  // WebGPU's representation of the available gpu hardware
+  const adapter = await navigator.gpu.requestAdapter(); // returns a promise, so use await
+  if (!adapter) {
     throw new Error("No appropriate GPUAdapter found.");
-}
+  }
 
-// The main interface through which most interaction with the GPU happens
-const device = await adapter.requestDevice();
+  // The main interface through which most interaction with the GPU happens
+  const device = await adapter.requestDevice();
 
-// Configuring the canvas
-const context = canvas.getContext("webgpu");
-const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
-context.configure({
+  // Configuring the canvas
+  const context = canvas.getContext("webgpu");
+  const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
     device: device,
     format: canvasFormat,
-});
+  });
 
-// Creating the quad
-const HALF_WIDTH = 1.0;
-const vertices = new Float32Array([
+  // Creating the quad for rendering the texture
+  const HALF_WIDTH = 1.0;
+  const vertices = new Float32Array([
     //   X,    Y,
     -HALF_WIDTH, -HALF_WIDTH, // Triangle 1 (Blue)
     HALF_WIDTH, -HALF_WIDTH,
@@ -38,98 +55,96 @@ const vertices = new Float32Array([
     -HALF_WIDTH, -HALF_WIDTH, // Triangle 2 (Red)
     HALF_WIDTH, HALF_WIDTH,
     -HALF_WIDTH, HALF_WIDTH,
-]);
+  ]);
 
-// Creating the buffer
-const vertexBuffer = device.createBuffer({
+  // Creating the buffer
+  const vertexBuffer = device.createBuffer({
     label: "Cell vertices",
     size: vertices.byteLength,
     usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-});
+  });
 
-// Copying the vertices into the buffer's memory
-device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+  // Copying the vertices into the buffer's memory
+  device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
 
-// Defining the vertex data structure
-const vertexBufferLayout = {
+  // Defining the vertex data structure
+  const vertexBufferLayout = {
     arrayStride: 8,
     attributes: [{
-        format: "float32x2",
-        offset: 0,
-        shaderLocation: 0, // Position, see vertex shader
+      format: "float32x2",
+      offset: 0,
+      shaderLocation: 0, // Position, see vertex shader
     }],
-};
+  };
 
-// Creating the shaders (they get passed in as strings)
-const cellShaderModule = device.createShaderModule({
-  label: 'Cell shader',
-  code: `
-    struct VertexInput {
-      @location(0) pos: vec2f,
-    };
-    struct VertexOutput {
-      @builtin(position) pos: vec4f,
-    };
-    @vertex
-    fn vertexMain(input: VertexInput) ->
-      VertexOutput {
-      var output: VertexOutput;
-      output.pos = vec4f(input.pos, 0, 1);
-      return output;
+  // Create a uniform buffer that describes the grid.
+  const uniformArray = new Float32Array([X_RES, Y_RES]);
+  const uniformBuffer = device.createBuffer({
+    label: "Grid Uniforms",
+    size: uniformArray.byteLength,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  });
+  device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
+
+  
+  // Creating the shaders (they get passed in as strings)
+  const cellShaderModule = device.createShaderModule({
+    label: 'Cell shader',
+    code: shaderText
+  });
+
+  // Creating the render pipeline (conttrols how geometry is drawn, which shaders are used, etc.)
+  const cellPipeline = device.createRenderPipeline({
+    label: "Cell pipeline",
+    layout: "auto",
+    vertex: {
+      module: cellShaderModule,
+      entryPoint: "vertexMain",
+      buffers: [vertexBufferLayout]
+    },
+    fragment: {
+      module: cellShaderModule,
+      entryPoint: "fragmentMain",
+      targets: [{
+        format: canvasFormat
+      }]
     }
-    @fragment
-    fn fragmentMain(@builtin(position) pos: vec4<f32>) -> @location(0) vec4f {
-      let uv = (vec2(pos.x, pos.y) / 512.0 - 0.5) * 2.0;
-      let color = uv; // resolution;
-      let l = length(color);
-      if l < 0.5 {
-        return vec4(0.0, 0.0, 1.0, 0.0);
-      } else {
-        return vec4f(l, 1.0, 0.0, 1.0);
-      }
-    }
-  `
-});
+  });
 
-// Creating the render pipeline (conttrols how geometry is drawn, which shaders are used, etc.)
-const cellPipeline = device.createRenderPipeline({
-  label: "Cell pipeline",
-  layout: "auto",
-  vertex: {
-    module: cellShaderModule,
-    entryPoint: "vertexMain",
-    buffers: [vertexBufferLayout]
-  },
-  fragment: {
-    module: cellShaderModule,
-    entryPoint: "fragmentMain",
-    targets: [{
-      format: canvasFormat
-    }]
-  }
-});
+  const bindGroup = device.createBindGroup({
+    label: "Cell renderer bind group",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [{
+      binding: 0,
+      resource: { buffer: uniformBuffer }
+    }],
+  });
 
-// Provides an interface for recording GPU commands
-const encoder = device.createCommandEncoder();
+  // Provides an interface for recording GPU commands
+  const encoder = device.createCommandEncoder();
 
-const pass = encoder.beginRenderPass({
+  const pass = encoder.beginRenderPass({
     colorAttachments: [{
-        view: context.getCurrentTexture().createView(),
-        loadOp: "clear",
-        clearValue: { r: 0, g: 0, b: 0.4, a: 1 }, // New line
-        storeOp: "store",
+      view: context.getCurrentTexture().createView(),
+      loadOp: "clear",
+      clearValue: { r: 0, g: 0, b: 0.4, a: 1 }, // New line
+      storeOp: "store",
     }]
-});
+  });
 
-pass.setPipeline(cellPipeline);
-pass.setVertexBuffer(0, vertexBuffer);
+  pass.setPipeline(cellPipeline);
+  pass.setVertexBuffer(0, vertexBuffer);
 
-// pass.setBindGroup(0, bindGroup); // setting bind group
+  pass.setBindGroup(0, bindGroup); // New line!
 
-pass.draw(vertices.length / 2); // 6 vertices
+  pass.draw(vertices.length / 2); // 6 vertices
 
-// Ending the render pass
-pass.end()
+  // Ending the render pass
+  pass.end()
 
-// Finish the command buffer and immediately submit it.
-device.queue.submit([encoder.finish()]);
+  // Finish the command buffer and immediately submit it.
+  device.queue.submit([encoder.finish()]);
+}
+
+run()
